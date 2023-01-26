dbab3012289f036176725ba7b7926cb1175fb082
GuilhermeUrenha <guilherme.urenha@gmail.com>
2023-01-26 03:47:57 -0300

    Base transition ver.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Transition } from 'react-transition-group';
+
+
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
+	const [inProp, setInProp] = useState(false);
+	var name = ['G','U','I','L','H','E','R','M','E'];
+	var assembleDelay = 500;
+	var extension = ['.','r','a','r']
+	var typeDelay = 0;
+
+	useEffect (()=>{
+		setInProp(true);
+	}, []);
+	
+	function nameAssemble(letter){
+		const assembleTransition = {
+			entering: { marginTop: '-15vh' },
+			entered: { marginTop: '15vh' }
+		};
+		//var position;
+		return (<Transition in={inProp} timeout={assembleDelay+=100}>
+			{(state) =>(
+				<div
+					className='NameLetter'
+					style={{
+						transition: `margin-top 500ms`,
+						...assembleTransition[state]
+					}}
 				>
-					Learn React
-				</a>
-			</header>
+				{letter}
+				</div>
+			)}
+		</Transition>);
+	}
+
+	function extensionType(letter){
+		const typeTransition = {
+			entering: { opacity: 0 },
+			entered: { opacity: 1 }
+		};
+		return (<Transition in={inProp} timeout={typeDelay+=300}>
+			{(state) =>(
+				<div
+					className='NameLetter NameExtension'
+					style={{
+						transition: `opacity 100ms ease-in-out`,
+						opacity: 0,
+						...typeTransition[state]
+					}}
+				>
+				{letter}	
+				</div>
+			)}
+		</Transition>);
+	}
+
+	return (
+		<div className='App'>
+			<header className='App-header'></header>
+			<div id='Name'>
+				{name.map((letter) => nameAssemble(letter))}
+				{extension.map((letter) => extensionType(letter))}
+			</div>
 		</div>
 	);
 }
