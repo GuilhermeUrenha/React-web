ad55b9f97c3b5b713cf782b75faba3089a6065a8
GuilhermeUrenha <guilherme.urenha@gmail.com>
2023-02-01 01:25:17 -0300

    Github cards v1

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -11,41 +11,71 @@ export default function App() {
 		setInProp(true);
 	}, []);
 
-	useEffect(() => {
-		if(inProp) getGitHub();
-	}, [inProp])
+	const octokit = new Octokit({
+		auth: token
+	});
 
-	async function getGitHub() {
-		const octokit = new Octokit({
-			auth: token
-		});
-		var result = await octokit.request('GET /users/GuilhermeUrenha/repos', {});
-		document.getElementById('section').textContent = JSON.stringify(result, null, 4);
+	var repositories = new Map();
+	class Repository {
+		constructor(id, name, url, language, updated, visibility){
+			this.id = id;
+			this.name = name;
+			this.url = url;
+			this.language = language;
+			this.updated = updated;
+			this.visibility = visibility;
+			this.commits = 0;
+		}
+		async getCommits(request) {
+			var commitsInfo = await (await fetch(request)).json().then(c => commitsInfo = c);
+			this.commits = commitsInfo;
+			//document.getElementById('commits').textContent = commitArray.size;
+			return 0;
+		}
+	}
+	const gitHub = () => octokit.request('GET /users/GuilhermeUrenha/repos', {});
+	const ActAsync = ({ data, error, isLoading }) => {
+		if (isLoading) return '';
+		if (error) return `Something went wrong: ${error.message}`;
+
+		for (var repo of data.data){
+			let repository = new Repository(
+				repo.id,
+				repo.name,
+				repo.html_url,
+				repo.language,
+				repo.updated_at,
+				repo.visibility
+			);
+			repository.getCommits(repo.commits_url.replace('{/sha}',''));
+			repositories.set(repo.id, repository);
+		}
+		return [...repositories.entries()].map((r) => (<div key={r[0]} className={'githubCard'}>{r[1].name}</div>));
 	}
 
 	var assembleDelay = 500;
-	function nameAssemble(letter, extension){
+	function nameAssemble(letter, key, extension){
 		const startingPoint = Math.floor(Math.random() * 2);
 		const topStart = 0, bottomStart = 1;
 		var startPosition;
 
 		if(startingPoint === topStart)
-			startPosition = '-15vh';
+			startPosition = '-15vmin';
 		else if(startingPoint === bottomStart)
-			startPosition = '60vh';
+			startPosition = '60vmin';
 		const assembleTransition = {
 			entering: { 
 				marginTop: startPosition
 			},
 			entered: { 
-				marginTop: '15vh',
+				marginTop: '15vmin',
 				opacity: 1
 			}
 		};
-		return (<Transition in={inProp} timeout={assembleDelay+=100}>
+		return (<Transition in={inProp} key={key} timeout={assembleDelay+=100}>
 			{(state) =>(
 				<div
-				className={`NameLetter ${extension ? 'NameExtension' : ''}`}
+				className={`NameLetter${extension ? ' NameExtension' : ''}`}
 				style={{
 					marginTop: startPosition,
 					...assembleTransition[state]
@@ -63,11 +93,13 @@ export default function App() {
 	return (
 		<main>
 			<div id='Name'>
-				{name.map((letter) => nameAssemble(letter))}
-				{extension.map((letter) => nameAssemble(letter, true))}
+				{name.map((letter, key) => nameAssemble(letter, key))}
+				{extension.map((letter, key) => nameAssemble(letter, key, true))}
 			</div>
-			<section id='section'>
-				{(()=>{return (<div>yes</div>)})()}
+			<section className='githubInfo'>
+				<Async promiseFn={gitHub}>
+					{ActAsync}
+				</Async>
 			</section>
 		</main>
 	);
